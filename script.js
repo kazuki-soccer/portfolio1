d1ff8cb082677774b79904e9dcb703a6af278a9f
kazuki-soccer <68420398+kazuki-soccer@users.noreply.github.com>
2021-03-22 01:56:33 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+$(function(){
+
+$('#head').hide().fadeIn(5000);
+$('#descri').hide().fadeIn(5000);
+$('#name').hide().fadeIn(1500);
+
+
+
+
+$("nav").hide();
+$(".menubtn").click(function(){//メニューボタンをクリックしたとき
+    $("nav").toggle(300);//0.3秒で表示したり非表示にしたりする
+});
+
+
+
+
+
+
+
+
+
+
+
+});
